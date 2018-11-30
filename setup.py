7283a4820930edff5658313d7f850d1d76bd1e10
Tore Butlin <tb267@cam.ac.uk>
2018-11-30 19:53:39 +0000

    testing ways to share dependencies

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ requires = ['peakutils', 'numpy', 'scipy', 'pyqtgraph', 'pyaudio', 'matplotlib']
 
 setup(
     name='pydvma',
-    version='0.2.1',
+    version='0.3.0',
     install_requires=requires,
     packages=['pydvma','labs'],
     license='BSD 3-Clause License',
