efdd944e8e151403b2b8fbc9ce7834c427f03a43
Tore Butlin <tb267@cam.ac.uk>
2019-08-21 18:29:57 +0100

    first version of multi-channel modal fitting

--- pydvma/__init__.py
+++ pydvma/__init__.py
@@ -9,5 +9,5 @@ from .analysis import calculate_fft, calculate_cross_spectrum_matrix, calculate_
 from .analysis import calculate_tf, calculate_tf_averaged, multiply_by_power_of_iw, best_match, calculate_sonogram
 from .streams import Recorder, Recorder_NI, start_stream, REC, setup_output_NI, setup_output_soundcard, list_available_devices, get_devices_NI, get_devices_soundcard
 from .interactive_tools import InteractiveLogging, InteractiveView
-from .modal import modal_fit_single
+from .modal import modal_fit_single_channel, modal_fit_all_channels
 
--- pydvma/modal.py
+++ pydvma/modal.py
@@ -43,7 +43,7 @@ def f_3dB(f,G0):
 
 
 
-def modal_fit_single(tf_data,freq_range=None,channel=0,measurement_type='acc'):
+def modal_fit_single_channel(tf_data,freq_range=None,channel=0,measurement_type='acc'):
     '''
     Fit modal parameters for a single mode to data within specified freq_range
     '''
@@ -61,39 +61,40 @@ def modal_fit_single(tf_data,freq_range=None,channel=0,measurement_type='acc'):
     G0 = tf_data.tf_data[selected_range,channel]
     
     fn0,zn0 = f_3dB(f,G0)
-    print(zn0)
     
-    fn0i = np.argmax(np.abs(G0))
-    fn0 = f[fn0i]
-    zn0 = np.diff(freq_range)[0]/2/fn0
+    
+    
     an0 = np.max(np.abs(G0))*(2*np.pi*fn0)**2 * 2*zn0
-
-    
     pn0 = 0
-    Rr0 = 0
-    Ri0 = 0
+    Rk0 = np.max(np.abs(G0))/1e3
+    Rm0 = np.max(np.abs(G0))*((2*np.pi*fn0)**2)/1e3
     
     
-    x0 = np.array([an0,fn0,zn0,pn0,Rr0,Ri0])
-    print(x0)
+    x0 = np.array([fn0,zn0,an0,pn0,Rk0,Rm0])
     
-
-    bounds = ([-np.inf,freq_range[0],0,-np.pi/2,-np.inf,-np.inf],[np.inf,freq_range[1],np.inf,np.pi/2,np.inf,np.inf])
+    bounds = ([freq_range[0],0,-np.inf,-np.pi/2,0,0],[freq_range[1],1,np.inf,np.pi/2,np.inf,np.inf])
 #    bounds = ((-np.inf,np.inf),(freq_range[0],freq_range[1]),(0,np.inf),(-np.pi/2,np.pi/2),(-np.inf,np.inf),(-np.inf,np.inf))
     
     r = optimize.least_squares(f_residual,x0, bounds=bounds, max_nfev=1000, args=(f,G0,measurement_type))
 #    r = optimize.minimize(f_residual,x0, bounds=bounds, method='SLSQP', args=(f,G0,measurement_type))
-    
+    print('')
+    print('fn={:.4g} (Hz), zn={:.4g}, an={:.4g}, phase={:.4g} (deg)'.format(r.x[0],r.x[1],r.x[2],r.x[3]*180/np.pi))
+    print('')
+    if (np.abs(r.x[3])*180/np.pi > 60):
+        print('Phase is significant, check ''measurement_type'' setting is correct')
     return r
     
     
 
 def f_TF(x,f,measurement_type):
-    an = x[0]
-    fn = x[1]
-    zn = x[2]
+
+    fn = x[0]
+    zn = x[1]
+    an = x[2]
     pn = x[3]
-    R  = x[4] + 1j*x[5]
+#    R  = x[4] + 1j*x[5]
+    R1 = x[4]
+    R2 = x[5]
     
     wn = 2*np.pi*fn
     w = 2*np.pi*f
@@ -106,8 +107,8 @@ def f_TF(x,f,measurement_type):
     elif measurement_type == 'dsp':
         p = 0
     
-    G = an*np.exp(1j*pn)/(wn**2 + 2j*wn*zn*w - w**2)
-    G = G*((1j*w)**p) + R
+    G = an*np.exp(1j*pn)/(wn**2 + 2j*wn*zn*w - w**2) + R1 - R2/(w**2)
+    G = G*((1j*w)**p)
     
     return G
 
@@ -126,4 +127,139 @@ def f_residual(x,f,G0,measurement_type):
     return e
 
 
+
+#%%
+def f_TF_all_channels(x,f,measurement_type):
+
+    N_tfs = np.int((len(x)-2)/4)
+    
+    fn = x[0]
+    zn = x[1]
+    an = x[2:2+N_tfs].reshape(1,N_tfs)
+    pn = x[2+N_tfs:2+2*N_tfs].reshape(1,N_tfs)
+#    R  = x[4] + 1j*x[5]
+    R1 = x[2+2*N_tfs:2+3*N_tfs].reshape(1,N_tfs)
+    R2 = x[2+3*N_tfs:2+4*N_tfs].reshape(1,N_tfs)
+    
+    wn = 2*np.pi*fn
+    w = 2*np.pi*f
+    w = w.reshape(len(w),1)
+    
+    
+    if measurement_type == 'acc':
+        p = 2
+    elif measurement_type == 'vel':
+        p = 1
+    elif measurement_type == 'dsp':
+        p = 0
+
+    G = an*np.exp(1j*pn)/(wn**2 + 2j*wn*zn*w - w**2) + R1 - R2/(w**2)
+    G = G*((1j*w)**p)
+    
+    
+    
+    return G
+
+
+def f_residual_all_channels(x,f,G0,measurement_type):
+    
+    G = f_TF_all_channels(x,f,measurement_type)
+        
+    e = (G-G0)
+    #e = np.abs(e)
+    e = np.concatenate((np.real(e),np.imag(e)))
+    e = e.reshape(np.size(e))
+    
+    return e
+
+#%% MULTI-CHANNEL MODAL FIT
+def modal_fit_all_channels(tf_data_list,freq_range=None,measurement_type='acc'):
+    '''
+    Fit modal parameters for a single mode to data within specified freq_range.
+    
+    Assumes all tf_data in tf_data_list have same frequency axes
+    '''
+    
+    if measurement_type == 'acc':
+        p = 2
+    elif measurement_type == 'vel':
+        p = 1
+    elif measurement_type == 'dsp':
+        p = 0
+    
+    # Find out how many TFs in dataset
+    N_tfs = 0
+    for tf_data in tf_data_list:
+        N_tfs += len(tf_data.tf_data[0,:])
+    
+    if freq_range == None:
+        freq_range = tf_data.freq_axis[[0,-1]]
+    
+    # get selected frequency axis
+    f = tf_data_list[0].freq_axis
+    selected_range = np.where((f > freq_range[0]) & (f < freq_range[1]))
+    
+    f = f[selected_range]
+    
+    # compile transfer functions into single array, and get initial guesses
+    G0 = np.zeros((len(f),N_tfs),dtype=complex)
+    fn0 = np.zeros(N_tfs)
+    zn0 = np.zeros(N_tfs)
+    counter = -1
+    for tf_data in tf_data_list:
+        for n_chan in range(len(tf_data.tf_data[0,:])):
+            counter += 1
+            G0[:,counter] = tf_data.tf_data[selected_range,n_chan]
+            fn0[counter],zn0[counter] = f_3dB(f,G0[:,counter])
+
+    # initial global guess for fn0,zn0 discarding any outliers
+    fn0 = np.median(fn0)
+    zn0 = np.median(zn0)
+    
+    # initial guesses for each channel
+    an0 = np.zeros(N_tfs)
+    pn0 = np.zeros(N_tfs)
+    Rk0 = np.zeros(N_tfs)
+    Rm0 = np.zeros(N_tfs)
+    counter = -1
+    for tf_data in tf_data_list:
+        for n_chan in range(len(tf_data.tf_data[0,:])):
+            counter += 1
+            an0[counter] = np.max(np.abs(G0[:,counter]))*(2*np.pi*fn0)**(2-p) * 2*zn0
+            pn0[counter] = 0
+            Rk0[counter] = np.max(np.abs(G0[:,counter]))/1e3
+            Rm0[counter] = np.max(np.abs(G0[:,counter]))*((2*np.pi*fn0)**2)/1e3
+    
+    
+    x0 = np.concatenate(([fn0],[zn0],an0,pn0,Rk0,Rm0))
+    print(x0)
+    
+    # bounds
+    B_fn = freq_range
+    B_zn = [0,1]
+    B_an = np.zeros((N_tfs,2))
+    B_an[:,0] = -np.inf
+    B_an[:,1] =  np.inf
+    B_pn = np.zeros((N_tfs,2))
+    B_pn[:,0] = -np.pi/2
+    B_pn[:,1] =  np.pi/2
+    B_rk = np.zeros((N_tfs,2))
+    B_rk[:,0] = 0
+    B_rk[:,1] = np.inf
+    B_rm = np.copy(B_rk)
+    
+    lower_bounds = np.concatenate(([B_fn[0]],[B_zn[0]],B_an[:,0],B_pn[:,0],B_rk[:,0],B_rm[:,0]))
+    upper_bounds = np.concatenate(([B_fn[1]],[B_zn[1]],B_an[:,1],B_pn[:,1],B_rk[:,1],B_rm[:,1]))
+    
+    bounds = (lower_bounds,upper_bounds)
+    
+    r = optimize.least_squares(f_residual_all_channels,x0, bounds=bounds, max_nfev=1000, args=(f,G0,measurement_type))
+#    r = optimize.minimize(f_residual,x0, bounds=bounds, method='SLSQP', args=(f,G0,measurement_type))
+    print('')
+    print('fn={:.4g} (Hz), zn={:.4g}, an={:.4g}, phase={:.4g} (deg)'.format(r.x[0],r.x[1],r.x[2],r.x[3]*180/np.pi))
+    print('')
+    if (np.abs(r.x[3])*180/np.pi > 60):
+        print('Phase is significant, check ''measurement_type'' setting is correct')
+    return r
+    
     
