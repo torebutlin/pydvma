753d5721037f400e19c1369af7456915bd166948
torebutlin <tb267@cam.ac.uk>
2019-09-19 15:21:37 +0100

    version 0.6.6

--- pydvma/datastructure.py
+++ pydvma/datastructure.py
@@ -16,7 +16,7 @@ import uuid
 import copy
 
 #%% version
-VERSION = '0.6.5' # keep in sync with setup.py
+VERSION = '0.6.6' # keep in sync with setup.py
 
 def update_dataset(dataset):
     dataset_new = DataSet()
