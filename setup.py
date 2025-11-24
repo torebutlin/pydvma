f132e5ced8e725397df6606829a7f58e1f1ad029
Tore Butlin <tb267@cam.ac.uk>
2025-11-24 12:46:28 +0000

    Bump version to 1.0.0 in datastructure.py and setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requires = ['peakutils', 'numpy', 'scipy', 'pyqtgraph', 'matplotlib', 'seaborn',
 
 setup(
     name='pydvma',
-    version='0.9.9', # keep in sync with datastructure.py
+    version='1.0.0', # keep in sync with datastructure.py
     install_requires=requires,
     packages=['pydvma'],
     package_data={'': ['icon.png']},
